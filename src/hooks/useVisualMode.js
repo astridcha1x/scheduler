ac79b118f7b41ff83cee24eeb1641e83fd861321
Astrid Aguilar <mixaguilar@yahoo.com>
2021-06-28 13:03:06 -0700

    update useVisualMode.js to prevent state from mutating

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -6,16 +6,15 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
 
-  const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
   const transition = (newMode, replace) => {
 
+    // react will rationalize the extra setState so no meaningful performance hit
     if (replace) {
       back();
     }
 
-    setMode(newMode);
     setHistory(prev => ([...history, newMode]));
 
   };
@@ -23,13 +22,19 @@ export default function useVisualMode(initial) {
   // go back to previous mode
   const back = () => {
 
-    if (history.length > 1) {
-      history.pop();
-      setMode(history[history.length - 1]);
-      setHistory(history);
+    if (history.length < 2) {
+      return;
     }
 
+    setHistory(prev => {
+      const newHistory = [...prev];
+      newHistory.pop(); 
+      return newHistory;
+    }); // can use slice instead, however, was recommended that I keep this for simplicity sake
+
   };
+
+  const mode = history.slice(-1)[0];
 
   return { mode, transition, back };
 
